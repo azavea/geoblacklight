2f5ef46df87388b4392a8ab5b730f1cb1dd5ca69
Stephen Balogh <sgbalogh@gmail.com>
2016-08-04 15:01:07 -0400

    refactors relations class, makes associated changes in partial

--- lib/geoblacklight/relations.rb
+++ lib/geoblacklight/relations.rb
@@ -1,42 +1,30 @@
 module Geoblacklight
   class Relations
+    attr_accessor :ancestors, :descendants, :search
+
     def initialize(document)
       @document = document
-      solr = RSolr.connect :url => ENV['SOLR_URL']
+      @solr = RSolr.connect :url => ENV['SOLR_URL']
+      @ancestors = find_ancestors
+      @descendants = find_descendants
+      # @search for providing link to faceted search results of all descendant records
+      @search = {:descendants => "catalog?f[#{Settings.FIELDS.SOURCE}][]=#{@document['layer_slug_s']}"}
+    end
 
-      @ancestors = []
+    def find_ancestors
       unless @document[Settings.FIELDS.SOURCE].blank?
-        @ancestors = solr.get 'select', :params => {:fq => "layer_slug_s:#{@document[Settings.FIELDS.SOURCE].join(' OR layer_slug_s:')}", :fl => ['dc_title_s', 'layer_slug_s']}
+        ancestors = @solr.get 'select', :params => {:fq => "layer_slug_s:#{@document[Settings.FIELDS.SOURCE].join(' OR layer_slug_s:')}", :fl => [Settings.FIELDS.TITLE, 'layer_slug_s']}
       end
-
-      @descendants = solr.get 'select', :params => {:q => "dct_source_sm:#{@document['layer_slug_s']}", :fl => ['dc_title_s', 'layer_slug_s']}
-
-      @count = {}
-      @count[:ancestors] = @ancestors['response']['numFound'] unless @ancestors.blank?
-      @count[:descendants] = @descendants['response']['numFound'] unless @descendants['response']['numFound'] == 0
-
-      @search = {}
-      @search[:descendants] = "catalog?f[#{Settings.FIELDS.SOURCE}][]=#{@document['layer_slug_s']}"
+      ancestors['response']['docs'] unless ancestors.blank?
     end
 
-    def ancestors
-      @ancestors['response']['docs'] unless @ancestors.blank?
-    end
-
-    def descendants
-      @descendants['response']['docs']
-    end
-
-    def count
-      @count
+    def find_descendants
+      descendants = @solr.get 'select', :params => {:q => "#{Settings.FIELDS.SOURCE}:#{@document['layer_slug_s']}", :fl => [Settings.FIELDS.TITLE, 'layer_slug_s']}
+      descendants['response']['docs'] unless descendants.blank?
     end
 
     def exist?
-      !@count.blank?
-    end
-
-    def search
-      @search
+      !@ancestors.blank? || !@descendants.blank?
     end
 
   end
